664958ec17211a7973936e8fed6587171c94bbd7
RevBase28 <reihanreinladimctna@gmail.com>
2020-12-26 23:27:13 +0700

    Test

--- main.c
+++ main.c
@@ -6,17 +6,12 @@ void showBoard(){
     printf("___________________\n");
     printf("|     |     |     |\n");
     printf("|  0  |  0  |  0  |\n");
-
     printf("|_____|_____|_____|\n");
     printf("|     |     |     |\n");
-
     printf("|  0  |  0  |  0  |\n");
-
     printf("|_____|_____|_____|\n");
     printf("|     |     |     |\n");
-
     printf("|  0  |  0  |  0  |\n");
-
     printf("|_____|_____|_____|\n\n");
 }
 
